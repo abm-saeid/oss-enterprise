9dc3bd02b57538dc64efdde5806c25d2c324e61d
abm-saeid <abmshaeid@gmail.com>
2020-04-25 05:42:58 +0600

    Added docstrings and comments

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from database import chapters, creatures, names, objects, places, plants, spells
+__author__="Rafeed M. Bhuyian"
+__version__='0.1Alpha'
+""" How to use:
+    Suppose you need to find a spell for crossword,
+    imput 'sp',
+    if the word is 3 letters and the 3rd letter is 'x',
+    then imput '3 3 x'
+    Output ['nox'] is your answer.
+    Warning!
+    There is a 80% possibility of multiple results."""
+from database import chapters, creatures, names, objects, places, plants, spells #imports
+
 def answer(lst):
+    """The real function"""
     while True:
         blank_lst=[]
         a,b,c=map(str,input().split())
@@ -15,6 +27,7 @@ def answer(lst):
     return lst[0]
 
 def main():
+    """ The other func"""
     the_dict={'sp':spells,'char':names,'cr':creatures,'ob':objects,'pl':plants,'chap':chapters,'place':places}
     while True:
         choice=input("What do you want to find: ")
